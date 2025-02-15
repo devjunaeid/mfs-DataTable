eade91743ac0580a02cd9609291e2149cd65d77c
devjunaeid <junaeidthamid@gmail.com>
2025-02-15 19:59:28 +0600

    init

---
+++ src/views/DataTableView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>This is from data table page..</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>This is from home..</h1>
+  </div>
+</template>
